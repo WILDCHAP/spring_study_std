efad862fe12640aaa53fa5e18eba0b1f2173cbfa
WILDCHAP <993704315@qq.com>
2020-10-08 19:26:57 +0800

    2020/10/08

--- spring_02/wildchap-01/src/test/java/MyTest.java
+++ spring_02/wildchap-01/src/test/java/MyTest.java
@@ -1,9 +1,11 @@
+import com.wildchap.spring1.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
-        
+        Student student = context.getBean("student", Student.class);
+        System.out.println(student.toString());
     }
 }
