c9988900d09b6341f274e88d5a3627c8b927dded
WILDCHAP <993704315@qq.com>
2020-10-10 21:38:58 +0800

    2020/10/10

--- spring_02/wildchap-05-aop/src/main/java/com/wildchap/log/DiyLog.java
+++ spring_02/wildchap-05-aop/src/main/java/com/wildchap/log/DiyLog.java
@@ -1,7 +1,9 @@
 package com.wildchap.log;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -11,11 +13,21 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class DiyLog {
     @Before("execution(* com.wildchap.userservlet.UserServletImpl.*(..))")
-    public void before(){
+    public void before() {
         System.out.println("在操作执行前");
     }
+
     @After("execution(* com.wildchap.userservlet.UserServletImpl.*(..))")
-    public void after(){
+    public void after() {
         System.out.println("在操作执行后");
     }
+
+    //在环绕增强中，我们可以给定一个参数，代长我们要获取处理切入的点;
+    @Around("execution(* com.wildchap.userservlet.UserServletImpl.*(..))")
+    public void around(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("环绕前");
+        Object proceed = jp.proceed();     //执行方法
+        System.out.println("环绕后");
+    }
+
 }
